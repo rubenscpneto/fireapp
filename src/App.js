2b9fbc017222962522e384cf3be331569aba4e4d
rubenscpneto <rubenscpneto@hotmail.com>
2025-01-31 15:45:38 -0300

    implement CRUD operations for posts with Firebase integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { db } from "./firebaseConnection";
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore';
+import { 
+  doc, 
+  setDoc, 
+  collection, 
+  addDoc, 
+  getDoc, 
+  getDocs, 
+  updateDoc, 
+  deleteDoc 
+} from 'firebase/firestore';
 
 import './app.css';
 
 function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+  const [idPost, setIdPost] = useState('');
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=> {
+    async function loadPosts() {
+      
+    }
+
+    loadPosts();
+
+  }, [])
 
   async function handleAdd(){
     // await setDoc(doc(db, "posts", "12345"), {
@@ -35,15 +56,59 @@ function App() {
   }
 
   async function buscarPost() {
-    const postRef = doc(db, "posts", "x8Qs6obBlewgFwtQzoLj")
+    // const postRef = doc(db, "posts", "x8Qs6obBlewgFwtQzoLj")
 
-    await getDoc(postRef)
-    .then((snapshot)=>{
-      setAutor(snapshot.data().autor)
-      setTitulo(snapshot.data().titulo)
+    // await getDoc(postRef)
+    // .then((snapshot)=>{
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
+    // })
+    // .catch(()=>{
+    //   console.log("ERRO AO BUSCAR")
+    // })
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista= [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+      setPosts(lista);
     })
-    .catch(()=>{
-      console.log("ERRO AO BUSCAR")
+    .catch((error) => {
+      console.log("DEU ALGUM ERRO AO BUSCAR")
+    })
+  }
+
+  async function editarPost(){
+    const docRef = doc(db, "posts", idPost)
+    await updateDoc(docRef, {
+      titulo: titulo,
+      autor: autor
+    })
+    .then(()=>{
+      console.log("Post atualizado");
+      setIdPost('');
+      setTitulo('');
+      setAutor('');
+    })
+    .catch((error)=>{
+      console.log("ERRO" + error)
+    })
+  }
+
+  async function excluirPost(id){
+    const docRef = doc(db, "posts", id)
+    await deleteDoc(docRef)
+    .then(()=>{
+      alert("POST DELETADO COM SUCESSO")
     })
   }
 
@@ -52,6 +117,14 @@ function App() {
       <h1>ReactJS + Firebase :)</h1>
 
       <div className="container">
+
+        <label>ID do post:</label>
+        <input
+          placeholder='Digite o ID do post'
+          value={idPost}
+          onChange={(e)=> setIdPost(e.target.value)}
+        />  <br/>
+
         <label>Titulo:</label>
         <textarea 
           type="text"
@@ -69,7 +142,22 @@ function App() {
         />
 
         <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={buscarPost}>Buscar post</button>
+        <button onClick={buscarPost}>Buscar post</button> <br/>
+
+        <button onClick={editarPost}>Atualizar post</button>
+
+        <ul>
+          {posts.map((post) => {
+            return(
+              <li key={post.id}>
+                <strong>ID: {post.id}</strong> <br/>
+                <span>Titulo: {post.titulo}</span> <br/>
+                <span>Autor: {post.autor}</span> <br/>
+                <button onClick={() => excluirPost(post.id)}>Excluir</button> <br/> <br/>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </div>
   );
